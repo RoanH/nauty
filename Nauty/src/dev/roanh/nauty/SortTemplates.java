180d532fda837d030729ca926aa11f4e2a6e4ec8
Roan Hofland <roan@roanh.dev>
2026-06-20 17:13:38 +0200

    Parallel sort

--- Nauty/src/dev/roanh/nauty/SortTemplates.java
+++ Nauty/src/dev/roanh/nauty/SortTemplates.java
@@ -77,7 +77,7 @@ public class SortTemplates{
 					break;
 				}
 				
-				sortSwap1(x, xb, xc);
+				sortSwap(x, xb, xc);
 				++xb;
 				--xc;
 			}
@@ -188,7 +188,7 @@ public class SortTemplates{
 			while(true){
 				while(xb <= xc && y[x[xb]] <= v){
 					if(y[x[xb]] == v){
-						sortSwap1(x, xa, xb);
+						sortSwap(x, xa, xb);
 						++xa;
 					}
 					++xb;
@@ -196,7 +196,7 @@ public class SortTemplates{
 
 				while(xc >= xb && y[x[xc]] >= v){
 					if(y[x[xc]] == v){
-						sortSwap1(x, xc, xd);
+						sortSwap(x, xc, xd);
 						--xd;
 					}
 					--xc;
@@ -206,7 +206,7 @@ public class SortTemplates{
 					break;
 				}
 
-				sortSwap1(x, xb, xc);
+				sortSwap(x, xb, xc);
 				++xb;
 				--xc;
 			}
@@ -220,7 +220,7 @@ public class SortTemplates{
 			}
 
 			for(xl = x0, xh = xb - s; s > 0; --s){
-				sortSwap1(x, xl, xh);
+				sortSwap(x, xl, xh);
 				++xl;
 				++xh;
 			}
@@ -234,7 +234,7 @@ public class SortTemplates{
 			}
 
 			for(xl = xb, xh = x0 + (nn - s); s > 0; --s){
-				sortSwap1(x, xl, xh);
+				sortSwap(x, xl, xh);
 				++xl;
 				++xh;
 			}
@@ -262,17 +262,159 @@ public class SortTemplates{
 			}
 		}
 	}
+	
+	public static void sortparallel(int[] x, int xOffset, int[] y, int yOffset, int n){
+		int i, j;
+		int a, d, ba, dc, s, nn;
+		int tmp2, y0, ya, yb, yc, yd, yl, yh;
+		int tmp1, v, v1, v2, v3;
+		int x0, xa, xb, xc, xd, xh, xl;
+
+		StackEntry[] stack = new StackEntry[40];
+		int top;
+
+		top = 0;
+		if(n > 1){
+			stack[top] = new StackEntry(xOffset, n);
+			++top;
+		}
+
+		while(top > 0){
+			--top;
+			x0 = stack[top].addr;
+			y0 = yOffset + (x0 - xOffset);
+			nn = stack[top].len;
+
+			if(nn < SORT_MINPARTITION){
+				for(i = 1; i < nn; ++i){
+					tmp1 = x[x0 + i];
+					tmp2 = y[y0 + i];
+					for(j = i; x[x0 + j - 1] > tmp1;){
+						x[x0 + j] = x[x0 + j - 1];
+						y[y0 + j] = y[y0 + j - 1];
+						if(--j == 0){
+							break;
+						}
+					}
+					x[x0 + j] = tmp1;
+					y[y0 + j] = tmp2;
+				}
+				continue;
+			}
+
+			if(nn < SORT_MINMEDIAN9){
+				v = sortMedianOf3(x[x0], x[x0 + nn / 2], x[x0 + nn - 1]);
+			}else{
+				v1 = sortMedianOf3(x[x0], x[x0 + 1], x[x0 + 2]);
+				v2 = sortMedianOf3(x[x0 + nn / 2 - 1], x[x0 + nn / 2], x[x0 + nn / 2 + 1]);
+				v3 = sortMedianOf3(x[x0 + nn - 3], x[x0 + nn - 2], x[x0 + nn - 1]);
+				v = sortMedianOf3(v1, v2, v3);
+			}
+
+			xa = xb = x0;
+			xc = xd = x0 + (nn - 1);
+			ya = yb = y0;
+			yc = yd = y0 + (nn - 1);
+			while(true){
+				while(xb <= xc && xb <= v){
+					if(xb == v){
+						xb = xa;
+						xa = v;
+						++xa;
+						sortSwap(y, ya, yb);
+						++ya;
+					}
+					++xb;
+					++yb;
+				}
+				while(xc >= xb && xc >= v){
+					if(xc == v){
+						xc = xd;
+						xd = v;
+						--xd;
+						sortSwap(y, yc, yd);
+						--yd;
+					}
+					--xc;
+					--yc;
+				}
+				if(xb > xc){
+					break;
+				}
+				sortSwap(x, xb, xc);
+				sortSwap(y, yb, yc);
+				++xb;
+				++yb;
+				--xc;
+				--yc;
+			}
+
+			a = xa - x0;
+			ba = xb - xa;
+			if(ba > a){
+				s = a;
+			}else{
+				s = ba;
+			}
+			for(xl = x0, xh = xb - s, yl = y0, yh = yb - s; s > 0; --s){
+				xl = xh;
+				xh = v;
+				++xl;
+				++xh;
+				sortSwap(y, yl, yh);
+				++yl;
+				++yh;
+			}
+			d = xd - x0;
+			dc = xd - xc;
+			if(dc > nn - 1 - d){
+				s = nn - 1 - d;
+			}else{
+				s = dc;
+			}
+			for(xl = xb, xh = x0 + (nn - s), yl = yb, yh = y0 + (nn - s); s > 0; --s){
+				xh = xl;
+				xl = v;
+				++xl;
+				++xh;
+				sortSwap(y, yl, yh);
+				++yl;
+				++yh;
+			}
+
+			if(ba > dc){
+				if(ba > 1){
+					stack[top] = new StackEntry(x0, ba);
+					++top;
+				}
+				if(dc > 1){
+					stack[top] = new StackEntry(x0 + (nn - dc), dc);
+					++top;
+				}
+			}else{
+				if(dc > 1){
+					stack[top] = new StackEntry(x0 + (nn - dc), dc);
+					++top;
+				}
+				if(ba > 1){
+					stack[top] = new StackEntry(x0, ba);
+					++top;
+				}
+			}
+		}
+	}
 
 	private static int sortMedianOf3(int a, int b, int c){
 		return (a <= b) ? ((b <= c) ? b : ((c <= a) ? a : c)) : ((a <= c) ? a : ((c <= b) ? b : c));
 	}
 
-	private static void sortSwap1(int[] x, int i, int j){
-		int tmp = x[i];
-		x[i] = x[j];
-		x[j] = tmp;
+	private static void sortSwap(int[] arr, int x, int y){
+		int tmp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = tmp;
 	}
 
+	//TODO consider turning this into two local int[] arrays for better CPU locality
 	private static record StackEntry(int addr, int len){
 	}
 }
