6598da21bfeac11c2b5c35a1dd91f1081a8786c5
Roan Hofland <roan@roanh.dev>
2026-06-28 16:25:01 +0200

    Dreadnaut tutorial

--- nauty/test/dev/roanh/nauty/NautyTest.java
+++ nauty/test/dev/roanh/nauty/NautyTest.java
@@ -13,6 +13,38 @@ import dev.roanh.nauty.struct.SparseGraph;
  */
 public class NautyTest{
 	private final NautyApi nauty = new NautyApi();
+	
+	/*
+	 * Creating expected test output with nauty's Dreadnaut (2.8.6):
+	 * 
+	 * General configuration:
+	 * > As
+	 * > d
+	 * > c
+	 * 
+	 * Vertex count and graph (example):
+	 * > n=4
+	 * > g
+	 *  0 : 1 3;
+	 *  1 : 0 2;
+	 *  2 : 1 3;
+	 *  3 : 0 2;
+	 * 
+	 * Execute:
+	 * > x
+	 * (1 3)
+	 * level 2:  3 orbits; 1 fixed; index 2
+	 * (0 1)(2 3)
+	 * level 1:  1 orbit; 0 fixed; index 4
+	 * 1 orbit; grpsize=8; 2 gens; 6 nodes; maxlev=3
+	 * canupdates=1; cpu time = 0.00 seconds
+	 * > b
+	 * 0 2 1 3      --> relabelling
+	 *  0 :  2 3;   \
+	 *  1 :  2 3;    | canonical graph
+	 *  2 :  0 1;    | relabelled and sorted
+	 *  3 :  0 1;   /
+	 */
 
 	@Test
 	public void graph1() throws InterruptedException{
@@ -29,8 +61,8 @@ public class NautyTest{
 			new int[]{2, 4}
 		});
 		
-		int[] lab = {0,1,2,3,4,5};
-		int[] ptn = {1,1,1,1,1,1};
+		int[] lab = new int[]{0, 1, 2, 3, 4, 5};
+		int[] ptn = new int[]{1, 1, 1, 1, 1, 1};
 		
 		assertArrayEquals(
 			new int[]{2, 1, 5, 3, 4, 0},
